f0e96261e7e170376b11b72532416a29be3f6a7a
Pedro Batuza <abraham_desmadroso@hotmail.com>
2020-12-08 18:08:53 -0600

    :rocket: uodate

--- Patrones Creacionales/Patron constructor/index.js
+++ Patrones Creacionales/Patron constructor/index.js
@@ -23,3 +23,17 @@ class MiClase {
 
 const instancia = new MiClase(4, 6);
 console.log('instancia', instancia);
+
+// Constructor con Objeto. 
+// class MiClase {
+//     constructor({p1, p2}){
+//         this.propiedad = p1;
+//         this.propiedad2 = p2;
+//         this.metodo = () => {
+//             console.log('metodo');
+//         }
+//     }
+// }
+
+// const instancia = new MiClase({p2:4, p1:6});
+// console.log('instancia', instancia);
