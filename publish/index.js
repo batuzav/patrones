53f3951a6ce9aed1405b898ba313d50f88bc1a57
Pedro Batuza <abraham_desmadroso@hotmail.com>
2021-01-16 14:56:05 -0600

    :rocket: adding publish patron..

---
+++ publish/index.js
@@ -0,0 +1,23 @@
+const user = new User();
+
+const init = () => {
+	user.on('login', userLoggedIn) 
+}
+
+const userLogginIn = () => {
+	//function operational
+}
+
+app.init();
+
+const login => () {
+	//logica de inicio
+
+user.tigger('login');
+
+}
+
+
+login();
+
+
