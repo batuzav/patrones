f69df0035b7a617455e0190eb82134c9497e8e07
Pedro Batuza <abraham_desmadroso@hotmail.com>
2021-01-16 00:05:48 -0600

    :rocket: facode complete

--- facade/index.js
+++ facade/index.js
@@ -1,31 +1,38 @@
 const https = require('https');
 
-const options = { 
-	host: "jsonplaceholder.typicode.com",
-	path: "/users",
-	method: "get",
-}
+const get = url => new Promise((resolvers, rej) => {
+	const camp = url.split('/');
+	const host = camp.shift();
+	const options = { 
+		host, 
+		path: `/${camp.join('/')}`,
+		method: "get",
+	}
 
-const req = https.request(options, res => {
-	res.setEncoding('utf-8')
-	let body = '';
+	const req = https.request(options, res => {
+		res.setEncoding('utf-8')
+		let body = '';
 	
-	res.on('data', d => {
-		body += d;
-	}); 
+		res.on('data', d => {
+			body += d;
+		}); 
 	
-	res.on('end', d => {
-		const parsed = JSON.parse(body);
-		console.log(parsed)
+		res.on('end', d => {
+			const parsed = JSON.parse(body);
+			resolvers(parsed)
+		});
 	});
+
+	req.on('error', (s) => {
+		rej(res);
+	});
+
+	req.end(); 
+
 });
 
-req.on('error', (s) => {
-	console.log(s);
-});
-
-req.end(); 
-
-
-
-
+const main = async () => {
+	const res =  await get('jsonplaceholder.typicode.com/users');
+	console.log(res)
+}
+main();
